557bc85c8be452cbe0aedc1814fedc5415b56c47
ashshaosh <ashshaosh@gmail.com>
2021-02-12 17:19:47 +0500

    inside/outside of goriutine addition

--- CONCURRENCY/goroutines/echoback.go
+++ CONCURRENCY/goroutines/echoback.go
@@ -8,17 +8,19 @@ import (
 )
 
 func main() {
+	go echo(os.Stdin, os.Stdout, "outside")
 	go func() {
 		fmt.Println("Inside goroutine")
-		go echo(os.Stdin, os.Stdout)
+		go echo(os.Stdin, os.Stdout, "inside")
 	}()
-	//go echo(os.Stdin, os.Stdout)
+
 	time.Sleep(30 * time.Second)
 	fmt.Println("Time is out")
 
 	os.Exit(0)
 }
 
-func echo(in io.Reader, out io.Writer) {
+func echo(in io.Reader, out io.Writer, where string) {
+	fmt.Println(where)
 	io.Copy(out, in)
 }
