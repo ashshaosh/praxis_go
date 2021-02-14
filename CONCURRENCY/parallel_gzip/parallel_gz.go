d3e12ccf04aabda92e70f50fd00cfe4840efac99
ashshaosh <ashshaosh@gmail.com>
2021-02-14 18:21:18 +0500

    parallel gzip with goroutines

--- CONCURRENCY/parallel_gzip/parallel_gz.go
+++ CONCURRENCY/parallel_gzip/parallel_gz.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"compress/gzip"
+	"fmt"
 	"io"
 	"os"
+	"sync"
 )
 
 func main() {
-	for _, file := range os.Args[1:] {
-		compress(file)
+	var wg sync.WaitGroup
+	var i int = -1
+	var file string
+
+	for i, file = range os.Args[1:] {
+		wg.Add(1)
+		go func(filename string) {
+			compress(filename)
+			fmt.Printf("Compressed %s file\n", filename)
+			wg.Done()
+		}(file)
 	}
+
+	wg.Wait()
+	fmt.Printf("Compressed %d files\n", i+1)
 }
 
 func compress(filename string) error {
--- CONCURRENCY/parallel_gzip/victims/README.md
+++
@@ -1,6 +0,0 @@
-# Go in Practice
-
-The source code repository for Manning Publications [Go in Practice](http://manning.com/butcher/).
-
-The source here is uncommented. To understand the source you'll need the
-commentary that goes along with it in the book.
