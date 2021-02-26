303b9b2fda231dcc78b9b1df565c68c03c837e28
ashshaosh <ashshaosh@gmail.com>
2021-02-26 10:03:14 +0500

    canary test example for type conformity

--- Debug&Test/06 - test for conformity/victim.go
+++ Debug&Test/06 - test for conformity/victim.go
@@ -1,5 +1,26 @@
-package main
+package canary
+
+import (
+	"io"
+	"testing"
+)
+
+type MyWriter struct{}
+
+func (m *MyWriter) Write([]byte) error { // must be  (int, error)
+	return nil // 0, nil
+}
 
 func main() {
+	m := map[string]interface{}{
+		"w": &MyWriter{},
+	}
+}
 
+func doSomething(m map[string]interface{}) {
+	w := m["w"].(io.Writer)
+}
+
+func TestWriter(t *testing.T) {
+	var _ io.Writer = &MyWriter{} // test will fail - wrong return type of F Write
 }
