6a0dce3b280a458a6cd585a6534c54c0e97ddff9
ashshaosh <ashshaosh@gmail.com>
2022-04-10 12:50:31 +0500

    Fixed names in MakePicGrayscale

--- MakePicGrayscale/makegray.go
+++ MakePicGrayscale/makegray.go
@@ -2,13 +2,13 @@ package main
 
 import "golang.org/x/tour/pic"
 
-// Pic func grayscale
-func Pic(dx, dy int) [][]uint8 {
-	slice2ret := make([][]uint8, dy)
+// MakeGrey func grayscale
+func MakeGrey(dotx, doty int) [][]uint8 {
+	slice2ret := make([][]uint8, doty)
 
-	for y := 0; y < dy; y++ {
-		sl := make([]uint8, dx)
-		for x := 0; x < dx; x++ {
+	for y := 0; y < doty; y++ {
+		sl := make([]uint8, dotx)
+		for x := 0; x < dotx; x++ {
 			sl[x] = uint8((x + y) / 2) // x*y, x^y, x/33 * y ...
 		}
 		slice2ret[y] = sl
@@ -18,5 +18,5 @@ func Pic(dx, dy int) [][]uint8 {
 }
 
 func main() {
-	pic.Show(Pic)
+	pic.Show(MakeGrey)
 }
